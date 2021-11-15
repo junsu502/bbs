76f7958d038b953d96dc1b99bf7b982568d754de
DESKTOP-P5FC9TB\junsu <nononojs2@gmail.com>
2021-11-15 12:28:17 +0900

    컨트롤러

--- Proj_MMS/src/mms/member/contoller/MemberController.java
+++ Proj_MMS/src/mms/member/contoller/MemberController.java
@@ -3,7 +3,7 @@ package mms.member.contoller;
 import java.util.Scanner;
 
 import mms.member.action.Action;
-//컨트롤러
+//컨트롤러 를 다시 컨트롤러
 public class MemberController {
 	public void processRequest(Action action,Scanner sc){
 		
