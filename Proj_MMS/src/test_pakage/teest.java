28ed5392e1ebecaa62baa67b5e6de8ba8eef7874
DESKTOP-P5FC9TB\junsu <nononojs2@gmail.com>
2021-11-15 12:31:39 +0900

    슬레이브

--- Proj_MMS/src/test_pakage/teest.java
+++ Proj_MMS/src/test_pakage/teest.java
@@ -4,7 +4,7 @@ public class teest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//ssssssssssssssssssssssssssssssss/
+		System.out.println("슬레이브 생성후 브랜치 연습");
 	}
 
 }
