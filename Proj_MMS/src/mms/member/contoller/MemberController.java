cad380f5fc7e7ab0992dfe3ddbcbe56549ba6dc9
junsu502 <94041577+junsu502@users.noreply.github.com>
2021-11-15 12:25:53 +0900

    Update MemberController.java
    컨트롤러수정

--- Proj_MMS/src/mms/member/contoller/MemberController.java
+++ Proj_MMS/src/mms/member/contoller/MemberController.java
@@ -3,7 +3,7 @@ package mms.member.contoller;
 import java.util.Scanner;
 
 import mms.member.action.Action;
-
+//컨트롤러
 public class MemberController {
 	public void processRequest(Action action,Scanner sc){
 		
